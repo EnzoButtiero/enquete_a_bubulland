d1c7c99ec652893bc517c8fd97a405bdb5c2868a
EnzoButtiero <94783966+EnzoButtiero@users.noreply.github.com>
2025-01-24 20:49:07 +0100

    based menu

---
+++ scripts/ui.gd
@@ -0,0 +1,52 @@
+extends CanvasLayer
+
+@onready var main_menu: Control = $MainMenu
+@onready var options_menu: Control = $OptionsMenu
+@onready var pause_menu: Control = $PauseMenu
+
+var main_menu_on : bool
+var options_menu_on : bool
+var pause_menu_on : bool
+
+var last_opened : Control
+
+func _ready() -> void:
+	main_menu_on = true
+	options_menu_on = false
+	pause_menu_on = false
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	main_menu.visible = main_menu_on
+	options_menu.visible = options_menu_on
+	pause_menu.visible = pause_menu_on
+	
+	if Input.is_action_just_released("pause") :
+		pause_menu_on = !pause_menu_on
+		last_opened = pause_menu
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
+
+func _on_main_options_pressed() -> void:
+	main_menu_on = !main_menu_on
+	options_menu_on = !options_menu_on
+	last_opened = main_menu
+
+func _on_pause_options_pressed() -> void:
+	pause_menu_on = !pause_menu_on
+	options_menu_on = !options_menu_on
+	last_opened = pause_menu
+
+func _on_start_pressed() -> void:
+	main_menu_on = false
+
+func _on_options_back_pressed() -> void:
+	options_menu_on = !options_menu_on
+	if last_opened == main_menu:
+		main_menu_on = !main_menu_on
+	elif last_opened == pause_menu:
+		pause_menu_on = !pause_menu_on
+
+func _on_pause_back_pressed() -> void:
+	pause_menu_on = !pause_menu_on
