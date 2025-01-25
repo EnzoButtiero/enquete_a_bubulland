9fa953661340c05e5656e1f111173be08b1279a8
EnzoButtiero <94783966+EnzoButtiero@users.noreply.github.com>
2025-01-25 23:55:46 +0100

    mini jeu boutique

--- scripts/boutique.gd
+++ scripts/boutique.gd
@@ -1,9 +1,15 @@
 extends Node2D
+const SARDINE_1 = preload("res://assets/sprites/sardine/sardine_1.png")
+const SARDINE_2 = preload("res://assets/sprites/sardine/sardine_2.png")
+const SARDINE_3 = preload("res://assets/sprites/sardine/sardine_3.png")
+const SARDINE_4 = preload("res://assets/sprites/sardine/sardine_4.png")
 
 @onready var timer: Timer = $Timer
-
+@onready var animation_player: AnimationPlayer = $Npc/AnimationPlayer
+@onready var npc: Sprite2D = $Npc
 @export var text : Control
-@export var npc : Sprite2D
+@onready var label: Label = $Npc/Label
+@onready var score_t: Label = $Score
 
 var score : int
 var mistake : int
@@ -20,32 +26,50 @@ func _ready() -> void:
 	activate_item()
 
 func _process(delta: float) -> void:
+	score_t.text = str(score)
 	if mistake == 3:
 		lose()
 	elif timer.time_left == 0:
 		win()
 
 func activate_item():
+	var rand_sard = randi_range(0,3)
+	match rand_sard:
+		0:
+			npc.texture = SARDINE_1
+		1:
+			npc.texture = SARDINE_2
+		2:
+			npc.texture = SARDINE_3
+		3:
+			npc.texture = SARDINE_4
+	animation_player.play("arrived")
 	var rand = randi_range(0,5)
 	
 	match rand :
 		0:
 			disable_all()
+			label.text="flacon à bulles"
 			cans=true
 		1:
 			disable_all()
+			label.text="algue"
 			seaweed=true
 		2:
 			disable_all()
+			label.text="savon"
 			savon=true
 		3:
 			disable_all()
+			label.text="statue"
 			statue=true
 		4:
 			disable_all()
+			label.text="bible"
 			bible=true
 		5:
 			disable_all()
+			label.text="papier bulle"
 			poppaper=true
 
 func disable_all():
@@ -67,53 +91,65 @@ func lose():
 func _on_cans_pressed() -> void:
 	if cans:
 		score += 15
+		animation_player.play("exited")
 		activate_item()
 	else:
 		score -= 30
 		mistake += 1
+		animation_player.play("exited")
 		activate_item()
 
 func _on_seaweed_pressed() -> void:
 	if seaweed:
 		score += 15
+		animation_player.play("exited")
 		activate_item()
 	else:
 		score -= 30
 		mistake += 1
+		animation_player.play("exited")
 		activate_item()
 
 func _on_savon_pressed() -> void:
 	if savon:
 		score += 15
+		animation_player.play("exited")
 		activate_item()
 	else:
 		score -= 30
 		mistake += 1
+		animation_player.play("exited")
 		activate_item()
 
 func _on_statue_pressed() -> void:
 	if statue:
 		score += 15
+		animation_player.play("exited")
 		activate_item()
 	else:
 		score -= 30
 		mistake += 1
+		animation_player.play("exited")
 		activate_item()
 
 func _on_bible_pressed() -> void:
 	if bible:
 		score += 15
+		animation_player.play("exited")
 		activate_item()
 	else:
 		score -= 30
 		mistake += 1
+		animation_player.play("exited")
 		activate_item()
 
 func _on_poppaper_pressed() -> void:
-	if bible:
+	if poppaper:
 		score += 15
+		animation_player.play("exited")
 		activate_item()
 	else:
 		score -= 30
 		mistake += 1
+		animation_player.play("exited")
 		activate_item()
