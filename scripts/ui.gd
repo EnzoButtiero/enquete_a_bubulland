be3c13d4eab42b8a2029d67b81633fc76d11dc57
EnzoButtiero <94783966+EnzoButtiero@users.noreply.github.com>
2025-01-25 20:46:17 +0100

    sdg

--- scripts/ui.gd
+++ scripts/ui.gd
@@ -1,5 +1,5 @@
 extends CanvasLayer
-const PLAYER = preload("res://scenes/prefabs/player.tscn")
+
 @onready var main_menu: Control = $MainMenu
 @onready var options_menu: Control = $OptionsMenu
 @onready var pause_menu: Control = $PauseMenu
@@ -13,7 +13,6 @@ var last_opened : Control
 var player
 
 func _ready() -> void:
-	player = PLAYER.instantiate()
 	main_menu_on = true
 	options_menu_on = false
 	pause_menu_on = false
@@ -43,7 +42,6 @@ func _on_pause_options_pressed() -> void:
 
 func _on_start_pressed() -> void:
 	main_menu_on = false
-	self.add_child(player)
 
 func _on_options_back_pressed() -> void:
 	options_menu_on = !options_menu_on
